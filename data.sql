d9fe4aefec139271b696e387f09fd025f9f51e6f
Prajwal Mavkar <mavkarprajwal@gmail.com>
2026-01-01 18:33:03 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,207 @@
+-- Insert data into customers table
+
+INSERT INTO customers
+(customer_id, name, city, email, phone, signup_date)
+VALUES
+(1,'Amit Sharma','Pune','amit1@gmail.com','9000000001','2023-01-01'),
+(2,'Riya Patil','Mumbai','riya2@gmail.com','9000000002','2023-01-03'),
+(3,'Rahul Verma','Delhi','rahul3@gmail.com','9000000003','2023-01-05'),
+(4,'Sneha Iyer','Chennai','sneha4@gmail.com','9000000004','2023-01-07'),
+(5,'Kunal Mehta','Ahmedabad','kunal5@gmail.com','9000000005','2023-01-09'),
+(6,'Neha Kulkarni','Pune','neha6@gmail.com','9000000006','2023-01-11'),
+(7,'Rohit Singh','Mumbai','rohit7@gmail.com','9000000007','2023-01-13'),
+(8,'Anjali Rao','Bangalore','anjali8@gmail.com','9000000008','2023-01-15'),
+(9,'Suresh Nair','Kochi','suresh9@gmail.com','9000000009','2023-01-17'),
+(10,'Pooja Shah','Surat','pooja10@gmail.com','9000000010','2023-01-19'),
+
+(11,'Vikas Joshi','Pune','vikas11@gmail.com','9000000011','2023-02-01'),
+(12,'Komal Jain','Indore','komal12@gmail.com','9000000012','2023-02-03'),
+(13,'Manish Gupta','Jaipur','manish13@gmail.com','9000000013','2023-02-05'),
+(14,'Kavita Rao','Hyderabad','kavita14@gmail.com','9000000014','2023-02-07'),
+(15,'Nikhil Patil','Nashik','nikhil15@gmail.com','9000000015','2023-02-09'),
+(16,'Priya Malhotra','Delhi','priya16@gmail.com','9000000016','2023-02-11'),
+(17,'Arjun Kapoor','Mumbai','arjun17@gmail.com','9000000017','2023-02-13'),
+(18,'Shweta Deshmukh','Pune','shweta18@gmail.com','9000000018','2023-02-15'),
+(19,'Deepak Yadav','Noida','deepak19@gmail.com','9000000019','2023-02-17'),
+(20,'Isha Khandelwal','Bhopal','isha20@gmail.com','9000000020','2023-02-19'),
+
+(21,'Akash Mishra','Kanpur','akash21@gmail.com','9000000021','2023-03-01'),
+(22,'Simran Kaur','Chandigarh','simran22@gmail.com','9000000022','2023-03-03'),
+(23,'Varun Saxena','Meerut','varun23@gmail.com','9000000023','2023-03-05'),
+(24,'Ritu Arora','Delhi','ritu24@gmail.com','9000000024','2023-03-07'),
+(25,'Mohit Aggarwal','Panipat','mohit25@gmail.com','9000000025','2023-03-09'),
+
+(26,'Sunita Devi','Patna','sunita26@gmail.com','9000000026','2023-03-11'),
+(27,'Rajesh Kumar','Ranchi','rajesh27@gmail.com','9000000027','2023-03-13'),
+(28,'Naina Gupta','Lucknow','naina28@gmail.com','9000000028','2023-03-15'),
+(29,'Siddharth Jain','Udaipur','siddharth29@gmail.com','9000000029','2023-03-17'),
+(30,'Mehul Patel','Vadodara','mehul30@gmail.com','9000000030','2023-03-19'),
+
+(31,'Farhan Ali','Bhopal','farhan31@gmail.com','9000000031','2023-04-01'),
+(32,'Alok Tiwari','Prayagraj','alok32@gmail.com','9000000032','2023-04-03'),
+(33,'Tanya Singh','Patiala','tanya33@gmail.com','9000000033','2023-04-05'),
+(34,'Harsh Vardhan','Gurgaon','harsh34@gmail.com','9000000034','2023-04-07'),
+(35,'Kriti Malhotra','Delhi','kriti35@gmail.com','9000000035','2023-04-09'),
+
+(36,'Abhishek Roy','Kolkata','abhishek36@gmail.com','9000000036','2023-04-11'),
+(37,'Payal Sinha','Ranchi','payal37@gmail.com','9000000037','2023-04-13'),
+(38,'Aman Bansal','Hisar','aman38@gmail.com','9000000038','2023-04-15'),
+(39,'Rakesh Pandey','Rewa','rakesh39@gmail.com','9000000039','2023-04-17'),
+(40,'Divya Chawla','Ambala','divya40@gmail.com','9000000040','2023-04-19'),
+
+(41,'Gaurav Arora','Delhi','gaurav41@gmail.com','9000000041','2023-05-01'),
+(42,'Monika Verma','Raipur','monika42@gmail.com','9000000042','2023-05-03'),
+(43,'Keshav Singh','Agra','keshav43@gmail.com','9000000043','2023-05-05'),
+(44,'Rupali Patil','Kolhapur','rupali44@gmail.com','9000000044','2023-05-07'),
+(45,'Ajay Naik','Goa','ajay45@gmail.com','9000000045','2023-05-09'),
+
+(46,'Sonal Jain','Ujjain','sonal46@gmail.com','9000000046','2023-05-11'),
+(47,'Nitesh Gupta','Jhansi','nitesh47@gmail.com','9000000047','2023-05-13'),
+(48,'Reema Das','Silchar','reema48@gmail.com','9000000048','2023-05-15'),
+(49,'Yogesh Pawar','Satara','yogesh49@gmail.com','9000000049','2023-05-17'),
+(50,'Bhavna Kulkarni','Pune','bhavna50@gmail.com','9000000050','2023-05-19');
+
+-- Insert data into products table
+
+INSERT INTO products
+(product_id, product_name, category, price)
+VALUES
+(101,'Laptop','Electronics',50000),
+(102,'Smartphone','Electronics',20000),
+(103,'Tablet','Electronics',30000),
+(104,'Smart Watch','Accessories',8000),
+(105,'Headphones','Accessories',3000),
+(106,'Bluetooth Speaker','Accessories',6000),
+(107,'Keyboard','Accessories',1500),
+(108,'Mouse','Accessories',800),
+(109,'Monitor','Electronics',12000),
+(110,'Printer','Electronics',9000),
+
+(111,'Power Bank','Accessories',2000),
+(112,'Router','Electronics',3500),
+(113,'Webcam','Accessories',2500),
+(114,'SSD','Electronics',7000),
+(115,'Hard Disk','Electronics',6000),
+(116,'Pen Drive','Accessories',1000),
+(117,'Gaming Chair','Furniture',15000),
+(118,'Office Chair','Furniture',9000),
+(119,'Desk','Furniture',12000),
+(120,'Laptop Bag','Accessories',2500),
+
+(121,'Mobile Cover','Accessories',500),
+(122,'Tripod','Accessories',1800),
+(123,'Microphone','Electronics',4000),
+(124,'Projector','Electronics',25000),
+(125,'UPS','Electronics',5500),
+
+(126,'WiFi Extender','Electronics',3000),
+(127,'Cooling Pad','Accessories',1800),
+(128,'VR Headset','Electronics',22000),
+(129,'Graphics Card','Electronics',45000),
+(130,'RAM 16GB','Electronics',8000),
+
+(131,'RAM 8GB','Electronics',4000),
+(132,'CPU','Electronics',20000),
+(133,'Motherboard','Electronics',15000),
+(134,'Cabinet','Electronics',6000),
+(135,'SMPS','Electronics',5000),
+
+(136,'Earbuds','Accessories',3500),
+(137,'Smart TV','Electronics',40000),
+(138,'HDMI Cable','Accessories',700),
+(139,'Extension Board','Accessories',1200),
+(140,'Surge Protector','Accessories',2500),
+
+(141,'Scanner','Electronics',11000),
+(142,'Barcode Reader','Electronics',9000),
+(143,'POS Machine','Electronics',30000),
+(144,'Digital Camera','Electronics',28000),
+(145,'Action Camera','Electronics',32000),
+
+(146,'Drone','Electronics',60000),
+(147,'Laptop Stand','Accessories',2200),
+(148,'Touch Pad','Accessories',3500),
+(149,'Stylus Pen','Accessories',1800),
+(150,'Docking Station','Electronics',9500);
+
+
+-- Insert data into orders table
+
+INSERT INTO orders
+(order_id, customer_id, order_date, amount, order_status, payment_method)
+VALUES
+(1001,1,'2024-01-01',50000,'Delivered','Card'),
+(1002,2,'2024-01-03',20000,'Delivered','UPI'),
+(1003,3,'2024-01-05',30000,'Delivered','Cash'),
+(1004,4,'2024-01-07',8000,'Delivered','UPI'),
+(1005,5,'2024-01-09',15000,'Pending','Card'),
+
+(1006,6,'2024-01-11',6000,'Delivered','UPI'),
+(1007,7,'2024-01-13',12000,'Delivered','Card'),
+(1008,8,'2024-01-15',9000,'Cancelled','UPI'),
+(1009,9,'2024-01-17',7000,'Delivered','Cash'),
+(1010,10,'2024-01-19',25000,'Delivered','Card'),
+
+(1011,11,'2024-02-01',4000,'Delivered','UPI'),
+(1012,12,'2024-02-03',5500,'Delivered','NetBanking'),
+(1013,13,'2024-02-05',18000,'Delivered','Card'),
+(1014,14,'2024-02-07',22000,'Pending','UPI'),
+(1015,15,'2024-02-09',3000,'Delivered','Cash'),
+
+(1016,16,'2024-02-11',8000,'Delivered','UPI'),
+(1017,17,'2024-02-13',60000,'Delivered','Card'),
+(1018,18,'2024-02-15',9500,'Delivered','UPI'),
+(1019,19,'2024-02-17',12000,'Delivered','Card'),
+(1020,20,'2024-02-19',5000,'Cancelled','UPI'),
+
+(1021,21,'2024-03-01',20000,'Delivered','Card'),
+(1022,22,'2024-03-03',15000,'Delivered','UPI'),
+(1023,23,'2024-03-05',1800,'Delivered','Cash'),
+(1024,24,'2024-03-07',25000,'Delivered','Card'),
+(1025,25,'2024-03-09',32000,'Delivered','NetBanking'),
+
+(1026,26,'2024-03-11',40000,'Delivered','Card'),
+(1027,27,'2024-03-13',9000,'Delivered','UPI'),
+(1028,28,'2024-03-15',6000,'Delivered','Cash'),
+(1029,29,'2024-03-17',15000,'Pending','Card'),
+(1030,30,'2024-03-19',18000,'Delivered','UPI'),
+
+(1031,31,'2024-04-01',30000,'Delivered','Card'),
+(1032,32,'2024-04-03',7000,'Delivered','UPI'),
+(1033,33,'2024-04-05',22000,'Delivered','Card'),
+(1034,34,'2024-04-07',5000,'Cancelled','Cash'),
+(1035,35,'2024-04-09',12000,'Delivered','UPI'),
+
+(1036,36,'2024-04-11',45000,'Delivered','Card'),
+(1037,37,'2024-04-13',3500,'Delivered','UPI'),
+(1038,38,'2024-04-15',9000,'Delivered','Cash'),
+(1039,39,'2024-04-17',15000,'Delivered','Card'),
+(1040,40,'2024-04-19',20000,'Delivered','UPI'),
+
+(1041,41,'2024-05-01',28000,'Delivered','Card'),
+(1042,42,'2024-05-03',32000,'Delivered','UPI'),
+(1043,43,'2024-05-05',18000,'Delivered','Cash'),
+(1044,44,'2024-05-07',6000,'Delivered','UPI'),
+(1045,45,'2024-05-09',45000,'Delivered','Card'),
+
+(1046,46,'2024-05-11',22000,'Delivered','UPI'),
+(1047,47,'2024-05-13',8000,'Delivered','Cash'),
+(1048,48,'2024-05-15',15000,'Pending','UPI'),
+(1049,49,'2024-05-17',30000,'Delivered','Card'),
+(1050,50,'2024-05-19',9500,'Delivered','UPI');
+
+-- Insert data into order_items
+
+INSERT INTO order_items
+(order_item_id, order_id, product_id, quantity)
+VALUES
+(1,1001,101,1),(2,1002,102,1),(3,1003,103,1),(4,1004,104,1),(5,1005,117,1),
+(6,1006,106,1),(7,1007,109,1),(8,1008,110,1),(9,1009,114,1),(10,1010,124,1),
+(11,1011,116,1),(12,1012,125,1),(13,1013,128,1),(14,1014,129,1),(15,1015,121,2),
+(16,1016,130,1),(17,1017,146,1),(18,1018,150,1),(19,1019,133,1),(20,1020,107,1),
+(21,1021,132,1),(22,1022,118,1),(23,1023,121,3),(24,1024,124,1),(25,1025,145,1),
+(26,1026,137,1),(27,1027,142,1),(28,1028,111,1),(29,1029,119,1),(30,1030,134,1),
+(31,1031,143,1),(32,1032,138,2),(33,1033,139,1),(34,1034,116,2),(35,1035,127,1),
+(36,1036,129,1),(37,1037,105,1),(38,1038,112,1),(39,1039,123,1),(40,1040,140,1),
+(41,1041,144,1),(42,1042,128,1),(43,1043,136,1),(44,1044,107,1),(45,1045,146,1),
+(46,1046,132,1),(47,1047,121,2),(48,1048,150,1),(49,1049,137,1),(50,1050,113,1);
