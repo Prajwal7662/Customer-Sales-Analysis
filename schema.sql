f29df26935d8a5071db342d3b9864d513a24c202
Prajwal Mavkar <mavkarprajwal@gmail.com>
2026-01-01 18:31:31 +0530

    Create schema.sql

---
+++ schema.sql
@@ -0,0 +1,37 @@
+
+-- Create Tables
+
+CREATE TABLE customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(50),
+    city VARCHAR(50),
+    email VARCHAR(100),
+    phone VARCHAR(15),
+    signup_date DATE
+);
+
+CREATE TABLE products (
+    product_id INT PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(50),
+    price DECIMAL(10,2)
+);
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    amount DECIMAL(10,2),
+    order_status VARCHAR(20),
+    payment_method VARCHAR(30),
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE order_items (
+    order_item_id INT PRIMARY KEY,
+    order_id INT,
+    product_id INT,
+    quantity INT,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
